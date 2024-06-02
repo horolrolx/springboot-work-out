6cd6bc2453e9dbcfd0491a7dcdbc36e638e9d62e
ten4roid <46445480+xoxlo@users.noreply.github.com>
2024-06-02 17:15:04 +0900

    Add files via upload

---
+++ src/main/java/com/springboot/springboot_work_out/SpringbootWorkOutApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.springboot_work_out;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootWorkOutApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootWorkOutApplication.class, args);
+	}
+
+}
