0f77bff9479b366bfb218e56857a557e25757ff2
xoxlo <thdckdtjr8@gmail.com>
2024-06-02 19:14:07 +0900

    폴더 이동

---
+++ Repository연결/src/test/java/com/springboot/springboot_work_out/SpringbootWorkOutApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.springboot_work_out;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootWorkOutApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
